6a97e520346043504c51595647a398e50cc18831
junior <astronomi@gmail.com>
2020-09-19 17:38:22 -0300

    Config flatlist

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import {Text, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {FlatList, View, Text} from 'react-native';
 
+import api from '../../services/api';
 import Styles from './styles';
 
+export interface Pokemons {
+    id: number;
+    name: string;
+    url: string;
+}
+
 const Details: React.FC = ({navigation}) => {
+    const [pokeData, setPokeData] = useState<Pokemons[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [page, setPage] = useState(1);
+
+    const loadMore = () => {
+        setPage(page + 1);
+    };
+
+    useEffect(() => {
+        const loadPokemons = async () => {
+            setIsLoading(true);
+
+            api.get('pokemon?limit=100&offset=200').then((response) => {
+                setPokeData(response.data.results);
+            });
+
+            setIsLoading(false);
+        };
+
+        loadPokemons();
+    }, []);
+
     return (
         <Styles.Container>
-            <Text>Details</Text>
-            <Button
-                title="Go to Details... again"
-                onPress={() => navigation.navigate('Home')}
+            <Styles.Title>Lista de Pokemons</Styles.Title>
+            <Styles.PokemonsList
+                data={pokeData}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => <Text>{item.name}</Text>}
+                onEndReachedThreshold={1}
             />
         </Styles.Container>
     );
--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components/native';
+import {FlatList} from 'react-native';
+
+import {Pokemons} from './index';
 
 export default {
     Container: styled.View`
@@ -6,5 +9,19 @@ export default {
         justify-content: center;
         align-items: center;
         background: rgba(255, 255, 255, 1);
+
+        padding: 0 30px;
+        padding-bottom: 30px;
+    `,
+
+    Title: styled.Text`
+        font-size: 26px;
+        color: rgba(46, 34, 34, 1);
+        padding-top: 20;
+        padding-bottom: 20;
+    `,
+
+    PokemonsList: styled(FlatList as new () => FlatList<Pokemons>)`
+        width: 100%;
     `,
 };
