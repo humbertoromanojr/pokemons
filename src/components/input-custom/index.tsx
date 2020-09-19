3c38c08b856f95bcfe064b893569c2197aeeafa5
junior <astronomi@gmail.com>
2020-09-19 14:06:32 -0300

    Config difer between inputs default and password

--- src/components/input-custom/index.tsx
+++ src/components/input-custom/index.tsx
@@ -1,18 +1,68 @@
-import React from 'react';
-import { TextInputProps } from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, {useState} from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Styles from './styles';
 
-function InputCustom: React.FC = () => {
-    return (
-        <Styles.Container >
-            {icon && (
-                <Icon name={icon} size={20} color="rgba(255, 255, 255, 0.6)" />
-            )}
-            <Styles.TInput  />
-        </Styles.Container>
-    );
+interface PropsType {
+    name: string;
+    icon: string;
+    placeholder: string;
+    eye?: boolean;
+    havePassword?: boolean;
 }
 
-export default forwardRef(InputCustom);
+const InputCustom: React.FC<PropsType> = (props: PropsType) => {
+    const [iconPassword, setIconPassword] = useState('eye-off');
+    const [hidePassword, setHidePassword] = useState(true);
+
+    const _changeIcon = () => {
+        iconPassword !== 'eye-off'
+            ? (setIconPassword('eye-off'), setHidePassword(false))
+            : (setIconPassword('eye'), setHidePassword(true));
+    };
+
+    const renderInputsDefault = () => {
+        return (
+            <Styles.Container>
+                <Icon name={props.icon} size={30} color="#666" />
+                <Styles.TextInput
+                    placeholderTextColor="#666"
+                    placeholder={props.placeholder}
+                    keyboardAppearance="dark"
+                />
+            </Styles.Container>
+        );
+    };
+
+    const renderInputPassword = () => {
+        return (
+            <Styles.Container>
+                <>
+                    <Icon name={props.icon} size={30} color="#666" />
+                    <Styles.TextInput
+                        placeholderTextColor="#666"
+                        placeholder={props.placeholder}
+                        keyboardAppearance="dark"
+                        secureTextEntry={hidePassword}
+                    />
+                </>
+                {props.eye && (
+                    <Icon
+                        name={iconPassword}
+                        onPress={() => _changeIcon()}
+                        size={30}
+                        color="#666"
+                    />
+                )}
+            </Styles.Container>
+        );
+    };
+
+    return (
+        <>
+            {props.havePassword ? renderInputPassword() : renderInputsDefault()}
+        </>
+    );
+};
+
+export default InputCustom;
--- src/components/input-custom/styles.ts
+++ src/components/input-custom/styles.ts
@@ -2,21 +2,21 @@ import styled from 'styled-components/native';
 
 export default {
     Container: styled.View`
-        padding: 0 15px;
-        height: 46px;
-        background: rgba(0, 0, 0, 0.1);
-        border-radius: 4px;
+        padding: 0 16px;
+        width: 100%;
+        height: 60px;
+        background: #f1f1f1f1;
+        border-radius: 10px;
+        margin-bottom: 8px;
 
         flex-direction: row;
         align-items: center;
     `,
 
-    TInput: styled.TextInput.attrs({
-        placeholderTextColor: 'rgba(255, 255, 255, 0.8)',
-    })`
+    TextInput: styled.TextInput`
         flex: 1;
-        font-size: 15px;
+        font-size: 16px;
+        color: #222;
         margin-left: 10px;
-        color: #fff;
     `,
 };
