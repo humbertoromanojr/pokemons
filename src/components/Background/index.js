91dc242ac2702e2316ceead95574ee27eb74cb9f
junior <astronomi@gmail.com>
2020-09-27 01:08:18 -0300

    Create background

---
+++ src/components/Background/index.js
@@ -0,0 +1,9 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#ee4035', '#58585a'],
+})`
+  flex: 1;
+`;
+
