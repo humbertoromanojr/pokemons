41ac6b3e653d47aad99cc4cafd3ef64ac24d39dd
junior <astronomi@gmail.com>
2020-09-27 01:16:23 -0300

    Update component background

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -2,7 +2,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
 export default styled(LinearGradient).attrs({
-  colors: ['#ee4035', '#58585a'],
+  colors: ['#ee4035', '#35353f'],
 })`
   flex: 1;
 `;
